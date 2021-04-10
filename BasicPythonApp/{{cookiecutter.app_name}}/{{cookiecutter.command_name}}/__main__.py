d3d74e8ce651cfbfeed0963158dba868ef5a0eec
Matt Wiley <wileymab@gmail.com>
2021-04-09 21:40:25 -0400

    Initial BasicPythonApp template.

---
+++ BasicPythonApp/{{cookiecutter.app_name}}/{{cookiecutter.command_name}}/__main__.py
@@ -0,0 +1,7 @@
+class {{cookiecutter.app_name}}:
+
+    def execute(self):
+        print("hi! I'm {{cookiecutter.app_name}}")
+
+if __name__ == "__main__":
+    {{cookiecutter.app_name}}().execute()
