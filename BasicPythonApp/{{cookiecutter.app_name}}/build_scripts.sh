e9fdef6cb9e3478b2e511346c0c19f5fe01b4580
Matt Wiley <wileymab@gmail.com>
2021-04-12 18:16:13 -0400

    Externalized app version data to display in app.

--- BasicPythonApp/{{cookiecutter.app_name}}/build_scripts.sh
+++ BasicPythonApp/{{cookiecutter.app_name}}/build_scripts.sh
@@ -3,8 +3,13 @@
 function main() {
     for cmd in "${@}"; do 
         case "${cmd}" in
-            "clean") rm -rf build dist *.egg-info ;;
-            "build") python setup.py bdist_wheel ;;
+            "clean") 
+                rm -rf build dist *.egg-info
+                ;;
+            "build") 
+                python -m pip install -r requirements.txt
+                python setup.py bdist_wheel 
+                ;;
             *);;
         esac
     done
--- BasicPythonApp/{{cookiecutter.app_name}}/setup.py
+++ BasicPythonApp/{{cookiecutter.app_name}}/setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+import yaml
+
+
+
+with open('app_data.yml','r') as app_data_file:
+    app_data = yaml.load(app_data_file,yaml.SafeLoader)
 
 setup(
     name='{{cookiecutter.app_name}}',
-    version='0.0.1',
+    version=f"{app_data.get('version')}",
     url='',
     author='',
     author_email='',
